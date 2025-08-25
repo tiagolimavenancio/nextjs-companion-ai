6946c1d75c1bcc7132a69f208a8f08d8ba5a72db
Tiago Lima <tiagolima@osf.digital>
2025-08-25 15:05:46 -0300

    feat: Added the sign-in and sign-up. Added the navbar component. Added the mode theme.

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+function RootPage() {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
+
+export default RootPage;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "@/components/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <NavBar />
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default RootLayout;
